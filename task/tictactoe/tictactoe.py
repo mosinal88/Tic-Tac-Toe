8da7319a3fedbc898856171a5aa6dd0e403beca0
mosinal88 <69855970+mosinal88@users.noreply.github.com>
2021-03-31 12:45:08 +0300

    Initial commit

---
+++ task/tictactoe/tictactoe.py
@@ -0,0 +1,7 @@
+# write your code here
+enter_cells = input('Enter cells:')
+print("---------")
+print("| {} {} {} |".format(enter_cells[0], enter_cells[1], enter_cells[2]))
+print("| {} {} {} |".format(enter_cells[3], enter_cells[4], enter_cells[5]))
+print("| {} {} {} |".format(enter_cells[6], enter_cells[7], enter_cells[8]))
+print("---------")
